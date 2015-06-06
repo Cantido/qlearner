7f1585ba5a7722315faadb101eeb62a577614daa
Robert Richter <robert.c.richter@gmail.com>
2015-06-05 22:19:52 -0500

    Remove unused imports

--- src/test/java/qlearning/AgentTest.java
+++ src/test/java/qlearning/AgentTest.java
@@ -1,11 +1,10 @@
 package qlearning;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
 
 public class AgentTest {
 
