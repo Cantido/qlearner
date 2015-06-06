a166b1811b995c0e231ca026e1d29977145aad32
Robert Richter <robert.c.richter@gmail.com>
2015-06-05 23:03:07 -0500

    Add Javadoc

--- src/main/java/qlearning/ExplorationStrategy.java
+++ src/main/java/qlearning/ExplorationStrategy.java
@@ -6,7 +6,15 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import qlearning.domain.Quality;
 
-
+/**
+ * The algorithm that is used to determine the next {@link Action} to take.
+ */
 public interface ExplorationStrategy {
+    /**
+     * Pick the next {@link Action} to take
+     * 
+     * @param stateActionQualities {@link State}-{@link Action} pairs and their associated {@link Quality Qualities}
+     * @return the chosen action
+     */
     public Action getNextAction(Map<Pair<State, Action>, Quality> stateActionQualities);
 }
