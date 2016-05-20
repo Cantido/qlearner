ac79c4a72b8b8514fc74148d8b805eac97900e93
Robert Richter <robert.c.richter@gmail.com>
2016-05-20 17:37:50 -0500

    Shorted a Javadoc link

--- qlearner-core/src/main/java/io/github/cantido/qlearner/agent/Agent.java
+++ qlearner-core/src/main/java/io/github/cantido/qlearner/agent/Agent.java
@@ -41,7 +41,7 @@ import javax.annotation.concurrent.NotThreadSafe;
  * This is the object that will navigate and learn the problem space (the {@code Environment}). It
  * implements the Q-learning algorithm to pick the optimal {@code Action} to take while in each
  * {@code State} in order to maximize the accumulated {@link io.github.cantido.qlearner.algorithm.
- * model.Reward} value.
+ * model.Reward Reward} value.
  * </p>
  */
 @NotThreadSafe
