84f3b274b9e539edc94620327e26551a37c8d067
Robert Richter <robert.c.richter@gmail.com>
2016-05-20 17:37:40 -0500

    Add more elaboration to the Javadoc

--- qlearner-core/src/main/java/io/github/cantido/qlearner/agent/package-info.java
+++ qlearner-core/src/main/java/io/github/cantido/qlearner/agent/package-info.java
@@ -14,7 +14,57 @@
  */
 
 /**
- * Framework for an actor to perform a q-learning decision process.
+ * The {@link io.github.cantido.qlearner.agent.Agent Agent} class is the entry point to this
+ * library. It has only one method: {@link io.github.cantido.qlearner.agent.Agent#takeNextAction() 
+ * Agent.takeNextAction()}.
+ * <pre>
+ * Agent agent = ... ;
+ * 
+ * while(true) {
+ *     agent.takeNextAction();
+ * }</pre>
+ * 
+ * For real-world applications, you may want to allow some time for your actions to have an effect,
+ * and for your {@link io.github.cantido.qlearner.client.Environment Environment} to arrive at a
+ * new {@link io.github.cantido.qlearner.client.State State}:
+ * <pre>
+ * while(true) {
+ *     agent.takeNextAction();
+ *     TimeUnit.MINUTES.sleep(1);
+ * }</pre>
+ * If you are trying to reach a global state after which your program will exit, you will have to
+ * specify your own way of identifying that. Since your {@code Environment} is responsible for its
+ * own {@code State}, why not add a method to your {@code Environment}?
+ * <pre>
+ * MyEnvironment myEnvironment = ... ;
+ * 
+ * while(!myEnvironment.isAtGoalState()) {
+ *     agent.takeNextAction();
+ *     TimeUnit.MINUTES.sleep(1);
+ * }</pre>
+ * If you ever need to reset your {@code Agent} while preserving its learned behavior, call
+ * {@link io.github.cantido.qlearner.agent.Agent#reset Agent.reset()}. This is important when
+ * you are trying to train your agent to find a goal state. For instance, if you are trying to
+ * teach your {@code Agent} how to play a game, then you will need to call {@code Agent.reset()}
+ * once the game ends, before you start your next game. If you don't, then the {@code Agent} will
+ * learn that it can make a move at the end of the game that will put it back at the beginning
+ * of the game!
+ * <pre>
+ * while(isTraining) {
+ *     myGameEnvironment.startGame();
+ *     
+ *     while(!myGameEnvironment.isAtGoalState()) {
+ *         agent.takeNextAction();
+ *     }
+ *     // The game is over. Now, we have to reset the agent before we start the game again.
+ *     
+ *     agent.reset();
+ *     myGameEnvironment.endGame();
+ * }</pre>
+ * You can also create a brand new {@code Agent} object, just be sure to save the {@link io.github.
+ * cantido.qlearner.algorithm.model.QualityMap} that you created it with. The {@code QualityMap} is
+ * where the {@code Agent} stores its learned values, and represents the hard work you just
+ * performed.
  */
 @javax.annotation.ParametersAreNonnullByDefault
 package io.github.cantido.qlearner.agent;
