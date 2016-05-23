f64d5f4080b7095a59cda81c5ebbea62f19e6acc
Robert Richter <robert.c.richter@gmail.com>
2016-05-23 17:05:02 -0500

    Use JUnitParams instead of Theories runner

--- qlearner-core/src/test/java/io/github/cantido/qlearner/algorithm/model/LearningRateTest.java
+++ qlearner-core/src/test/java/io/github/cantido/qlearner/algorithm/model/LearningRateTest.java
@@ -15,56 +15,37 @@
 
 package io.github.cantido.qlearner.algorithm.model;
 
-import static org.apache.commons.lang3.math.NumberUtils.DOUBLE_ONE;
-import static org.apache.commons.lang3.math.NumberUtils.DOUBLE_ZERO;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.lessThan;
-import static org.hamcrest.Matchers.lessThanOrEqualTo;
-import static org.junit.Assume.assumeThat;
-
-import io.github.cantido.qlearner.algorithm.model.LearningRate;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Rule;
-import org.junit.experimental.theories.DataPoints;
-import org.junit.experimental.theories.Theories;
-import org.junit.experimental.theories.Theory;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 @SuppressWarnings({"null", "javadoc"})
-@RunWith(Theories.class)
+@RunWith(JUnitParamsRunner.class)
 public class LearningRateTest {
   @Rule
   public ExpectedException exception = ExpectedException.none();
-
-  @DataPoints
-  public static double[] doubles = new double[] {Double.NEGATIVE_INFINITY, (-Double.MIN_NORMAL),
-      DOUBLE_ZERO, Double.MIN_NORMAL, (DOUBLE_ONE - Double.MIN_NORMAL), DOUBLE_ONE,
-      (DOUBLE_ONE + Double.MIN_NORMAL), Double.POSITIVE_INFINITY};
-
-  @Theory
+  
+  @Test
+  @Parameters({"-0", "0", "0.1", "0.5", "0.9", "1"})
   public void learningRateBetweenZeroAndOneInclusiveIsLegal(double learningRate) {
-    assumeThat(learningRate, greaterThanOrEqualTo(DOUBLE_ZERO));
-    assumeThat(learningRate, lessThanOrEqualTo(DOUBLE_ONE));
-
     // No exception expected
     new LearningRate(learningRate);
   }
 
-  @Theory
+  @Test
+  @Parameters({"-0.1", "-1"})
   public void learningRateLessThanZeroIsIllegal(double learningRate) {
-    assumeThat(learningRate, lessThan(DOUBLE_ZERO));
-
     exception.expect(IllegalArgumentException.class);
     new LearningRate(learningRate);
   }
 
-  @Theory
+  @Test
+  @Parameters({"1.1", "2.0"})
   public void learningRateGreaterThanOneIsIllegal(double learningRate) {
-    assumeThat(learningRate, greaterThan(DOUBLE_ONE));
-
     exception.expect(IllegalArgumentException.class);
     new LearningRate(learningRate);
   }
-
 }
