fbb6e65ed54888588d28333c47af02b68acc1932
Robert Richter <robert.c.richter@gmail.com>
2016-05-23 16:58:16 -0500

    Add a lot of test cases for BackwardsInduction

--- qlearner-core/src/test/java/io/github/cantido/qlearner/algorithm/quality/BackwardInductionTest.java
+++ qlearner-core/src/test/java/io/github/cantido/qlearner/algorithm/quality/BackwardInductionTest.java
@@ -24,29 +24,96 @@ import io.github.cantido.qlearner.algorithm.model.Quality;
 import io.github.cantido.qlearner.algorithm.model.QualityUpdateStrategy;
 import io.github.cantido.qlearner.algorithm.model.Reward;
 import io.github.cantido.qlearner.algorithm.quality.BackwardInduction;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 @SuppressWarnings({"null", "javadoc"})
+@RunWith(JUnitParamsRunner.class)
 public class BackwardInductionTest {
   @Rule
   public MockitoRule mockito = MockitoJUnit.rule();
 
   private final QualityUpdateStrategy backwardInduction = new BackwardInduction();
-
+  
+  /*
+   * Using a lot of primes to improve my chances of getting unique values.
+   * 
+   * Each one of these values differs from the first by only one variable.
+   */
+  @SuppressWarnings("unused")
+  private Object[] newQualityValues() {
+    return new Object[] {
+      new Object[] {
+        new Quality(3),
+        new LearningRate(0.8),
+        new Reward(7),
+        new DiscountFactor(0.4),
+        new Quality(11),
+        new Quality(9.72)
+      },
+      new Object[] {
+        new Quality(13),          // Only difference from first
+        new LearningRate(0.8),
+        new Reward(7),
+        new DiscountFactor(0.4),
+        new Quality(11),
+        new Quality(11.72)
+      },
+      new Object[] {
+        new Quality(3),
+        new LearningRate(0.3),    // Only difference from first
+        new Reward(7),
+        new DiscountFactor(0.4),
+        new Quality(11),
+        new Quality(5.52)
+      },
+      new Object[] {
+        new Quality(3),
+        new LearningRate(0.8),
+        new Reward(17),           // Only difference from first
+        new DiscountFactor(0.4),
+        new Quality(11),
+        new Quality(17.72)
+      },
+      new Object[] {
+        new Quality(3),
+        new LearningRate(0.8),
+        new Reward(7),
+        new DiscountFactor(0.1),  // Only difference from first
+        new Quality(11),
+        new Quality(7.08)
+      },
+      new Object[] {
+        new Quality(3),
+        new LearningRate(0.8),
+        new Reward(7),
+        new DiscountFactor(0.4),
+        new Quality(13),          // Only difference from first
+        new Quality(10.36)
+      },
+    };
+  }
+  
   @Test
-  public void computeNewQuality() {
-    Quality oldQuality = new Quality(1.0);
-    LearningRate learningRate = new LearningRate(1.0);
-    Reward reward = new Reward(1.0);
-    DiscountFactor discountFactor = new DiscountFactor(1);
-    Quality optimalFutureValueEstimate = new Quality(1.0);
-
-    Quality expected = new Quality(2.0);
-    Quality actual = backwardInduction.next(oldQuality, learningRate, reward, discountFactor,
-        optimalFutureValueEstimate);
+  @Parameters(method = "newQualityValues")
+  public void computeNewQuality(
+                                Quality oldQuality,
+                                LearningRate learningRate, 
+                                Reward reward,
+                                DiscountFactor discountFactor,
+                                Quality optimalFutureValueEstimate,
+                                Quality expected) {
+    Quality actual = backwardInduction.next(
+                                            oldQuality,
+                                            learningRate,
+                                            reward,
+                                            discountFactor,
+                                            optimalFutureValueEstimate);
 
     assertThat(actual, is(expected));
   }
